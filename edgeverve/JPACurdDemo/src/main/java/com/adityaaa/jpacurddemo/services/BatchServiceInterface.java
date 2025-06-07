e0321e7d29715eed525becd5395dcc17225f96ed
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-06-07 12:07:04 +0530

    updated case2 and case 3 functionalities

--- edgeverve/JPACurdDemo/src/main/java/com/adityaaa/jpacurddemo/services/BatchServiceInterface.java
+++ edgeverve/JPACurdDemo/src/main/java/com/adityaaa/jpacurddemo/services/BatchServiceInterface.java
@@ -3,6 +3,7 @@ package com.adityaaa.jpacurddemo.services;
 import com.adityaaa.jpacurddemo.beans.Batches;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BatchServiceInterface {
 
@@ -10,6 +11,7 @@ public interface BatchServiceInterface {
 
     public void addMultipleBatches(List<Batches> batchesList);
 
+    public Optional<Batches> findBatchById(int batchId);
 
 //     public List<Batches> showAllBatches();
 }
