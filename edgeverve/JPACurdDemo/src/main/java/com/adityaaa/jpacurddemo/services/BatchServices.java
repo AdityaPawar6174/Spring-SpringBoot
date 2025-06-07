a3db32059cdb5f4e778ad07dd766a009afa96d32
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-06-07 13:50:30 +0530

    updated all the changes in the JPA CRUD APP

--- edgeverve/JPACurdDemo/src/main/java/com/adityaaa/jpacurddemo/services/BatchServices.java
+++ edgeverve/JPACurdDemo/src/main/java/com/adityaaa/jpacurddemo/services/BatchServices.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 @Service
 public class BatchServices implements BatchServiceInterface{
@@ -28,5 +29,50 @@ public class BatchServices implements BatchServiceInterface{
     public Optional<Batches> findBatchById(int batchId) {
         return batchRepo.findById(batchId);
     }
+
+    @Override
+    public void updateBatchesOnbasisOfId(int batchId) {
+
+        try {
+            Optional<Batches> optional = batchRepo.findById(batchId);
+            Batches batch = optional.get();
+
+            if (batch != null) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter New Start Date");
+                String newStartDate = sc.next();
+                System.out.println("Enter New End Date");
+                String newEndDate = sc.next();
+                batch.setBatchStartDate(newStartDate);
+                batch.setBatchEndDate(newEndDate);
+                batchRepo.save(batch);
+            }
+        }
+        catch (Exception e){
+            System.err.println("Batch Id not Found...! In the Update Batch In Service"+e);
+        }
+    }
+
+    @Override
+    public List<Batches> showAllBatches() {
+        List<Batches> batchesList =  batchRepo.findAll();
+        return batchesList;
+    }
+
+    @Override
+    public void removeBatchById(int batchId) {
+        try{
+            batchRepo.deleteById(batchId);
+        }
+        catch (Exception e){
+            System.err.println("Id is invalid..."+e);
+        }
+    }
+
+    @Override
+    public List<Batches> findOnBasisOfBatchName(String batchName) {
+        return batchRepo.findByBatchName(batchName);
+    }
+
 
 }
