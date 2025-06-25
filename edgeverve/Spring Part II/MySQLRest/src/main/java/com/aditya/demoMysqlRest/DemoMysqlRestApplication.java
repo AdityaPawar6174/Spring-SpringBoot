30b2f2acae9c56c20a595125dc820a947f46f47b
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-06-25 23:02:33 +0530

    Added git Rest project with MySQL & PostgreSQL Database Integration

---
+++ edgeverve/Spring Part II/MySQLRest/src/main/java/com/aditya/demoMysqlRest/DemoMysqlRestApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.demoMysqlRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMysqlRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMysqlRestApplication.class, args);
+	}
+
+}
---
+++ edgeverve/Spring Part II/MySQLRest/src/test/java/com/aditya/demoMysqlRest/DemoMysqlRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.demoMysqlRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMysqlRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
