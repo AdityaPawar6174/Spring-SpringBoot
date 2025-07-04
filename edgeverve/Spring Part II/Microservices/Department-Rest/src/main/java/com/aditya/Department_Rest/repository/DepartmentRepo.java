7b2aa33428beead5f8d3860688639aa327a6ea0e
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-04 20:32:11 +0530

    Removed unused microservice packages

--- edgeverve/Spring Part II/Microservices/Department-Rest/src/main/java/com/aditya/Department_Rest/repository/DepartmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.aditya.Department_Rest.repository;
-
-import com.aditya.Department_Rest.model.Departments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepo extends JpaRepository<Departments, Long> {
-}
