8daaa4f07155aa21c951fc0a1ef1cf9fa46cc994
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-04 20:38:55 +0530

    discarding deleted packages

--- edgeverve/Spring Part II/Microservices/Rest Template/Department-Rest/src/main/java/com/aditya/Department_Rest/service/DepartmentServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.aditya.Department_Rest.service;
-
-import com.aditya.Department_Rest.model.Departments;
-
-public interface DepartmentServiceInterface {
-    Departments saveDepartment(Departments departments);
-    Departments getDepartmentById(long id);
-}
--- edgeverve/Spring Part II/Microservices/Rest Template/User-Rest/src/main/java/com/aditya/User_Rest/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.aditya.User_Rest.repository;
-
-import com.aditya.User_Rest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User,Long> {
-}
