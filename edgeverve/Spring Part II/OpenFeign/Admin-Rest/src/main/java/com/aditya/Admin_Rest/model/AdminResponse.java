9633f859233b3cbe87b62570ef040d2c56dd26d8
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-07-04 20:15:26 +0530

    SpringBoot microservices using OpenFiegn Style...Project Admin-Rest is Added

---
+++ edgeverve/Spring Part II/OpenFeign/Admin-Rest/src/main/java/com/aditya/Admin_Rest/model/AdminResponse.java
@@ -0,0 +1,16 @@
+package com.aditya.Admin_Rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatusCode;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AdminResponse {
+    private HttpStatusCode httpStatusCode;
+    private String httpStatusMessage;
+}
